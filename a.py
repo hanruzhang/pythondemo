bc4ed36c9e0a140267a92d261b3fdc4391a856fd
hanruzhang <zhanghanru0124@163.com>
2021-02-02 00:48:43 +0800

    2021-02-02 modify

--- a.py
+++ a.py
@@ -1,3 +1,4 @@
+###
 print("hello python")
 print("hello world")
 
@@ -16,3 +17,40 @@ print(list(r1))
 
 
 #命令式编程
+
+
+#装饰器
+import time
+def f1():
+    print('This is a function')
+
+#unix 时间戳
+#f1()
+
+def f2():
+    print("this is a function2")
+
+#对修改是封闭的，对扩展是开放的
+def print_current_time(func):
+    print(time.time())
+    func()
+
+
+print_current_time(f1)
+print_current_time(f2)
+###
+
+#装饰器
+def decorator(func):
+    def wrapper():
+        print(time.time())
+        func()
+    return wrapper
+
+def f1():
+    print("this is a function")
+
+
+
+f = decorator(f1)
+f()
